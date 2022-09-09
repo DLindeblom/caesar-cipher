db327a674d799bed30f58a8f80e02995608151e6
DLindeblom <drdlindeblom@gmail.com>
2022-09-09 12:10:07 -0400

    initial commit, create a couple variables

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CaesarCipher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+      Console.Write("Please enter a secret message you would like encrypted: ");
+      string secretString = Console.ReadLine();
+
+      char[] secretMessage = new char [secretString.Length];
+
+      char[] encryptedMessage = new char [secretMessage.Length];
+
+      for (int i = 0; i < secretMessage.Length; i++)
+      {
+        
+      }
+    }
+  }
+}
